123d70ffe4726fda0ba8bf3eaa0e7aaf4491a3ab
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-16 22:07:05 +0000

    Main_Featured

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -7,12 +7,14 @@
                 <img src="images/img_hdr/pngegg.png" alt="" class="sneaker__img">
                 <span class="sneaker__name">Nike Jordan</span>
             </article>
+
             <article class="sneaker">
-                <img src="images/img_hdr/pngwing_2.png" alt="" class="sneaker__img">
+                <img src="images/img_hdr/pngwing_2.png" alt="" class="sneaker__img1">
                 <span class="sneaker__name">Nike Free RN</span>
             </article>
+
             <article class="sneaker">
-                <img src="images/img_hdr/pngwing_3.png" alt="" class="sneaker__img">
+                <img src="images/img_hdr/pngwing_3.png" alt="" class="sneaker__img2">
                 <span class="sneaker__name">Nike Air Force</span>
             </article>
         </div>
