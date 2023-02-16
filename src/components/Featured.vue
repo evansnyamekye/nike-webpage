ea081ac77a0ae9bec0b665ab24219fda471f55ef
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-16 23:14:07 +0000

    Third_commit

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -1,5 +1,24 @@
 <template>
- 
+    <section class="featured section" id="featured">
+        <h2 class="section-title">Featured</h2>
+
+        <div class="featured__container bd-grid">
+            <article class="sneaker">
+                <img src="images/img_hdr/pngegg.png" alt="" class="sneaker__img">
+                <span class="sneaker__name">Nike Jordan</span>
+            </article>
+
+            <article class="sneaker">
+                <img src="images/img_hdr/pngwing_2.png" alt="" class="sneaker__img1">
+                <span class="sneaker__name">Nike Free RN</span>
+            </article>
+
+            <article class="sneaker">
+                <img src="images/img_hdr/pngwing_3.png" alt="" class="sneaker__img2">
+                <span class="sneaker__name">Nike Air Force</span>
+            </article>
+        </div>
+</section>
 </template> 
 
 <script setup>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -23,44 +23,8 @@
         </nav>
 
     </header>
+    
 
-    <main class="I-main">
-        <section class="home" id="home">
-            <div class="home__container bd-grid">
-                <div class="home__sneaker">
-                    <div class="home__shape"></div>
-                    <img src="images/img_hdr/nike-air-force.png" alt="" class="home__img">
-                </div>
-                <div class="home__data">
-                    <span class="home__new">Nike Air</span>
-                    <p>Experience the New Nike Air</p>
-                    <a href="#" class="button">Explore now</a>
-                </div>
-
-            </div>
-        </section>
-
-        <section class="featured section" id="featured">
-            <h2 class="section-title">Featured</h2>
-
-            <div class="featured__container bd-grid">
-                <article class="sneaker">
-                    <img src="images/img_hdr/pngegg.png" alt="" class="sneaker__img">
-                    <span class="sneaker__name">Nike Jordan</span>
-                </article>
-
-                <article class="sneaker">
-                    <img src="images/img_hdr/pngwing_2.png" alt="" class="sneaker__img1">
-                    <span class="sneaker__name">Nike Free RN</span>
-                </article>
-
-                <article class="sneaker">
-                    <img src="images/img_hdr/pngwing_3.png" alt="" class="sneaker__img2">
-                    <span class="sneaker__name">Nike Air Force</span>
-                </article>
-            </div>
-        </section>
-    </main>
 </template>
 
 <script setup>
