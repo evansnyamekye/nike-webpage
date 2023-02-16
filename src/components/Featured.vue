5fd2028a6292786aaaa55ac194a8f498e00be4c2
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-16 21:52:36 +0000

    Home_Featured

--- src/components/Featured.vue
+++ src/components/Featured.vue
@@ -1,5 +1,14 @@
 <template>
-
+      <section class="featured section" id="featured">
+        <h2 class="section-title">Featured</h2>
+        
+        <div class="featured__container bd-grid">
+            <article class="sneaker">
+                <div class="sneaker__sale">Sale</div>
+                <img src="images/img_hdr/pngegg.png" alt="" class="">
+            </article>
+        </div>
+    </section>
 
 </template> 
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -24,14 +24,28 @@
 
     </header>
 
+    <main class="I-main">
+        <section class="home" id="home">
+            <div class="home__container bd-grid">
+                <div class="home__sneaker">
+                    <div class="home__shape"></div>
+                    <img src="images/img_hdr/nike-air-force.png" alt="" class="home__img">
+                </div>
 
+                <div class="home__data">
+                    <span class="home__new">Nike Air</span>
+                    <p>Experience the New Nike Air</p>
+                </div>
+
+            </div>
+        </section>
+
+</main>
 </template>
 
 <script setup>
 
 </script>  
  
-<style scoped>
-
-</style>
+<style scoped></style>
 
