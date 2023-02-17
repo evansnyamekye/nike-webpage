37192e779adf734e8e4413c07f47f60435e639e8
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-17 14:46:33 +0000

    Footer_js_commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -7,31 +7,48 @@
                     commu nication. Real innovations and
                     a positive.</p>
                 <div>
-                    <i class='bx bxl-twitter'></i>
-                    <i class='bx bxl-instagram'></i>
-                    <i class='bx bxl-youtube'></i>
+                    <a href="#" class="footer__social"><i class='bx bxl-twitter'></i></a>
+                    <a href="#" class="footer__social"><i class='bx bxl-instagram'></i></a>
+                    <a href="#" class="footer__social"><i class='bx bxl-youtube'></i></a>
                 </div>
             </div>
             <div class="footer__box">
                 <h3 class="footer__tilte">About</h3>
                 <ul>
-                    <li>Features</li>
-                    <li>FAQ'S</li>
-                    <li>News</li>
-                    <li>Pricing</li>
+                    <li><a href="#" class="footer__link"></a>Features</li>
+                    <li><a href="#" class="footer__link"></a>FAQ'S</li>
+                    <li><a href="#" class="footer__link"></a>News</li>
+                    <li><a href="#" class="footer__link"></a>Pricing</li>
                 </ul>
             </div>
             <div class="footer__box">
                 <h3 class="footer__tilte">Company</h3>
                 <ul>
-                    <li>Core values</li>
-                    <li>Partner w/ us</li>
-                    <li>Blog</li>
-                    <li>Contact</li>
+                    <li><a href="#" class="footer__link"></a>Core values</li>
+                    <li><a href="#" class="footer__link"></a>Partner w/ us</li>
+                    <li><a href="#" class="footer__link"></a>Blog</li>
+                    <li><a href="#" class="footer__link"></a>Contact</li>
                 </ul>
+            </div>
+            <div class="footer__box">
+                <h3 class="footer__tilte">Get in touch</h3>
+                <div class="button-ft">
+                    <a href="#" class="hero-btn-ft">Get Access</a>
+                </div>
             </div>
         </div>
     </footer>
+
+    <div class="wrapper row">
+        <div id="copyright" class="hoc clear">
+            <!-- ################################################################################################ -->
+            <p  class="footer__copy"><a href="#">TravelAgent</a>
+           Copyright &copy; 2022 - All Rights Reserved -
+          
+           Terms of Service <a target="_blank" href="#">Privacy Policy</a></p>
+            <!-- ################################################################################################ -->
+        </div>
+    </div>
 </template> 
 
 <script setup>
