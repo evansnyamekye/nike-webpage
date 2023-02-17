8d792b5703ebb9c2a02e6a0feaebf7869cba59ef
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-17 13:50:50 +0000

    Footer_third_commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,11 +6,29 @@
                 <p>We believe brand interaction is key in<br>
                     commu nication. Real innovations and
                     a positive.</p>
-                    <div>
-                        <i class='bx bxl-twitter'></i>
-                        <i class='bx bxl-instagram'></i>
-                        <i class='bx bxl-youtube'></i>
-                    </div>
+                <div>
+                    <i class='bx bxl-twitter'></i>
+                    <i class='bx bxl-instagram'></i>
+                    <i class='bx bxl-youtube'></i>
+                </div>
+            </div>
+            <div class="footer__box">
+                <h3 class="footer__tilte">About</h3>
+                <ul>
+                    <li>Features</li>
+                    <li>FAQ'S</li>
+                    <li>News</li>
+                    <li>Pricing</li>
+                </ul>
+            </div>
+            <div class="footer__box">
+                <h3 class="footer__tilte">Company</h3>
+                <ul>
+                    <li>Core values</li>
+                    <li>Partner w/ us</li>
+                    <li>Blog</li>
+                    <li>Contact</li>
+                </ul>
             </div>
         </div>
     </footer>
