7f4f4e75a57e1b9f2541b4f1883fd23758a88185
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-17 09:22:35 +0000

    third_invinci_commit

--- src/components/Invinci.vue
+++ src/components/Invinci.vue
@@ -2,6 +2,9 @@
     <section class="invinci_section" id="women">
         <h1>INTRODUCING THE NEW<br>NIKE INVINCEBLE 3</h1>
         <p>ZoomX cushioning designed to help you,stay on your feet today, tomorrow, and beyond</p>
+        <div class="hero-container">
+            <a href="#" class="hero-btn">Shop</a>
+        </div>
         <div class="invinci__img">
             <img src="images/banner/Rectangle_1.jpg" alt="">
         </div>
