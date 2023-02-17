5a1ea516a9859bdf62dc8980d3645ea13bced409
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-17 13:44:42 +0000

    Footer_script_commit

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,9 +3,14 @@
         <div class="footer__container bd-grid">
             <div class="footer__box">
                 <img src="images/footer/travel.png">
-                <p>We believe brand interaction is key in
+                <p>We believe brand interaction is key in<br>
                     commu nication. Real innovations and
                     a positive.</p>
+                    <div>
+                        <i class='bx bxl-twitter'></i>
+                        <i class='bx bxl-instagram'></i>
+                        <i class='bx bxl-youtube'></i>
+                    </div>
             </div>
         </div>
     </footer>
