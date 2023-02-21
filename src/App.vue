b07d6dbcd6ac0d312863e589c8f1ea98608347a6
evansnyamekye <60971417+evansnyamekye@users.noreply.github.com>
2023-02-21 22:38:26 +0000

    Footer_ css

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,6 @@
 import Header from './components/Header.vue'; 
 import Home from './components/Home.vue';
 import Featured from './components/Featured.vue';
-import Invinci from './components/Invinci.vue';
 import Popular from './components/Popular.vue';
 import Custome from './components/Custom.vue';
 import PopularCloth from './components/PopoluarCloth.vue';
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,53 +1,55 @@
 <template>
-     <section class="footer grid">
-                <div class="footer-logo frid">
-                    <img src="images/footer/travel.png" alt="" />
-                    <p>We believe brand interaction is key in<br/>
-                        communication. Real innovations and
-                        a positive.</p>
-                    <div class="social-media">
-                        <a href="#" class="footer__social"><i class='bx bxl-twitter'></i></a>
-                        <a href="#" class="footer__social"><i class='bx bxl-instagram'></i></a>
-                        <a href="#" class="footer__social"><i class='bx bxl-youtube'></i></a>
-                    </div>
-                 </div>
-            <div class="footer__box">
-                <h3 class="footer__tilte">About</h3>
-                <ul>
-                    <li><a href="#" class="footer__link"></a>Features</li>
-                    <li><a href="#" class="footer__link"></a>FAQ'S</li>
-                    <li><a href="#" class="footer__link"></a>News</li>
-                    <li><a href="#" class="footer__link"></a>Pricing</li>
-                </ul>
+    <section class="footer">
+        <div class="footer-logo">
+            <img src="images/footer/travel.png" alt="" />
+            <p>We believe brand interaction is key in<br />
+                communication. Real innovations and
+                a positive.</p>
+            <div class="social-media">
+                <a href="#" class="footer__social"><i class='bx bxl-twitter'></i></a>
+                <a href="#" class="footer__social"><i class='bx bxl-instagram'></i></a>
+                <a href="#" class="footer__social"><i class='bx bxl-youtube'></i></a>
             </div>
-            <div class="footer__box">
-                <h3 class="footer__tilte">Company</h3>
-                <ul>
-                    <li><a href="#" class="footer__link"></a>Core values</li>
-                    <li><a href="#" class="footer__link"></a>Partner w/ us</li>
-                    <li><a href="#" class="footer__link"></a>Blog</li>
-                    <li><a href="#" class="footer__link"></a>Contact</li>
-                </ul>
-            </div>
+        </div>
+        <div class="footer__menu">
+            <h3 class="footer__tilte">About</h3>
+            <ul>
+                <li><a href="#" class="footer__link">Features</a></li>
+                <li><a href="#" class="footer__link">FAQ'S</a></li>
+                <li><a href="#" class="footer__link">News</a></li>
+                <li><a href="#" class="footer__link">Pricing</a></li>
+            </ul>
 
-            <div class="footer__box">
-                <h3 class="footer__tilte">Get in touch</h3>
-                <div class="input-field">
-                    <input type="email" placeholder="Your email here...">
-                </div>
-                <div class="button-ft">
-                    <a href="#" class="hero-btn-ft">Get Access</a>
-                </div>
+        </div>
+        <div class="footer__menu">
+            <h3 class="footer__tilte">Company</h3>
+            <ul>
+                <li><a href="#" class="footer__link">Core values</a></li>
+                <li><a href="#" class="footer__link">Partner w/ us</a></li>
+                <li><a href="#" class="footer__link">Blog</a></li>
+                <li><a href="#" class="footer__link">Contact</a></li>
+            </ul>
+
+        </div>
+
+        <div class="footer__menu">
+            <h3 class="footer__tilte">Get in touch</h3>
+            <div class="input-field">
+                <input type="email" placeholder="Your email here...">
             </div>
-            </section>
+            <div class="button-ft">
+                <a href="#" class="hero-btn-ft">Get Access</a>
+            </div>
+        </div>
+    </section>
 
     <div class="footer__link">
         <div id="copyright" class="hoc clear">
             <!-- ################################################################################################ -->
-            <p  class=""><a href="#">TravelAgent</a>
-           Copyright &copy; 2022 - All Rights Reserved -
-          
-           Terms of Service <a target="_blank" href="#">Privacy Policy</a></p>
+            <p class=""><a href="#">TravelAgent</a>
+                Copyright &copy; 2022 - All Rights Reserved -
+
+                Terms of Service <a target="_blank" href="#">Privacy Policy</a></p>
             <!-- ################################################################################################ -->
         </div>
     </div>
